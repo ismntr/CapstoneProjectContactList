cf45b414608c2e57f17f6cdf684fc771e0b360f4
ismet <ismntr@icloud.com>
2023-10-29 23:57:40 +0300

    son değişiklikler

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import sqlite3
 import csv
 import json
+import os
 
+from simple_term_menu import TerminalMenu
 
+# Klasörleri oluşturmak için yardımcı fonksiyon
+def create_folders():
+    folders = ["SQLData", "CSVData", "JSONData", "IsmetifyData"]
+    for folder in folders:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
 
-# Create a connection to the SQLite database
-conn = sqlite3.connect("SQLData/mydatabase.db")
+# SQLite veritabanı oluşturma
+def create_sqlite_database():
+    conn = sqlite3.connect("SQLData/mydatabase.db")
+    cursor = conn.cursor()
 
-# Create a cursor
-cursor = conn.cursor()
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS contacts
+    cursor.execute('''CREATE TABLE IF NOT EXISTS contacts
                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
                    first_name TEXT,
                    last_name TEXT,
@@ -19,111 +26,233 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS contacts
                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
                    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP)''')
 
-# Değişiklikleri kaydet
-conn.commit()
+    conn.commit()
+    conn.close()
 
-def add_contact(first_name, last_name, phone_number, email):
-    # Check if a contact with the same name and email exists
+# CSV, JSON ve İsmetify dosyalarını oluşturma
+def create_data_files():
+    files = {
+        "CSVData/contacts.csv": ["first_name", "last_name", "phone_number", "email"],
+        "JSONData/contacts.json": [],
+        "IsmetifyData/contacts.txt": [],
+    }
+
+    for file, header in files.items():
+        if not os.path.exists(file):
+            with open(file, 'w', newline='') as f:
+                if header:
+                    writer = csv.writer(f)
+                    writer.writerow(header)
+
+# SQLite veritabanına kişi ekleme fonksiyonu
+def add_contact_to_sqlite(first_name, last_name, phone_number, email):
+    conn = sqlite3.connect("SQLData/mydatabase.db")
+    cursor = conn.cursor()
+
     cursor.execute("SELECT id FROM contacts WHERE first_name = ? AND email = ?", (first_name, email))
     existing_contact = cursor.fetchone()
 
     if existing_contact:
         print("A contact with the same name and email already exists.")
     else:
-        # Add a new contact
         cursor.execute("INSERT INTO contacts (first_name, last_name, phone_number, email) VALUES (?, ?, ?, ?)",
                        (first_name, last_name, phone_number, email))
         conn.commit()
-        print("Contact added successfully.")
+        conn.close()
+        print("Contact added successfully to SQLite.")
 
-# Call the add_contact function with the contact details
-add_contact("John", "Doe", "555-123-4567", "john.doe@example.com")
-
-# Close the database connection
-conn.close()
-
-
-# CSV verilerini okuma
-def read_csv_data(filename):
-    data = []
-    with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            data.append(row)
-    return data
-
-# CSV verilerini yazma
-def write_csv_data(filename, data):
-    with open(filename, 'w', newline='') as file:
+# CSV verilerine kişi ekleme fonksiyonu
+def add_contact_to_csv(first_name, last_name, phone_number, email):
+    new_contact = [first_name, last_name, phone_number, email]
+    with open("CSVData/contacts.csv", 'a', newline='') as file:
         writer = csv.writer(file)
-        for row in data:
-            writer.writerow(row)
+        writer.writerow(new_contact)
+    print("Contact added successfully to CSV.")
 
-# CSV verilerini güncelleme
-def update_csv_data(filename, row_index, column_index, new_value):
-    data = read_csv_data(filename)
-    data[row_index][column_index] = new_value
-    write_csv_data(filename, data)
+# JSON verilerine kişi ekleme fonksiyonu
+def add_contact_to_json(first_name, last_name, phone_number, email):
+    new_contact_dict = {"first_name": first_name, "last_name": last_name, "phone_number": phone_number, "email": email}
+    json_file = "JSONData/contacts.json"
 
-# CSV verilerini silme
-def delete_csv_data(filename, row_index):
-    data = read_csv_data(filename)
-    del data[row_index]
-    write_csv_data(filename, data)
+    if not os.path.exists(json_file):
+        with open(json_file, 'w') as file:
+            json.dump([new_contact_dict], file, indent=4)
+    else:
+        with open(json_file, 'r') as file:
+            try:
+                json_data = json.load(file)
+            except json.decoder.JSONDecodeError:
+                json_data = []
+
+        json_data.append(new_contact_dict)
+        with open(json_file, 'w') as file:
+            json.dump(json_data, file, indent=4)
+
+    print("Contact added successfully to JSON.")
 
 
-# JSON verilerini okuma
-def read_json_data(filename):
-    with open(filename, 'r') as file:
-        data = json.load(file)
-    return data
+# İsmetify verilerine kişi ekleme fonksiyonu
+def add_contact_to_ismetify(first_name, last_name, phone_number, email):
+    new_contact_str = "|".join([first_name, last_name, phone_number, email])
+    with open("IsmetifyData/contacts.txt", 'a') as file:
+        file.write(new_contact_str + '\n')
+    print("Contact added successfully to İsmetify.")
 
-# JSON verilerini yazma
-def write_json_data(filename, data):
-    with open(filename, 'w') as file:
-        json.dump(data, file, indent=4)
+# Kişi ekleme işlemlerini birleştiren fonksiyon
+def add_contact_to_all_sources(first_name, last_name, phone_number, email):
+    add_contact_to_sqlite(first_name, last_name, phone_number, email)
+    add_contact_to_csv(first_name, last_name, phone_number, email)
+    add_contact_to_json(first_name, last_name, phone_number, email)
+    add_contact_to_ismetify(first_name, last_name, phone_number, email)
 
-# JSON verilerini güncelleme
-def update_json_data(filename, key, new_value):
-    data = read_json_data(filename)
-    data[key] = new_value
-    write_json_data(filename, data)
+# Ana menü içindeki fonksiyonlar
+def list_contacts_menu():
+    contacts = list_contacts()
+    if not contacts:
+        print("Listelenecek kişi bulunamadı.")
+    else:
+        for contact in contacts:
+            print("Ad: {}, Soyad: {}, Telefon: {}, E-posta: {}".format(contact[1], contact[2], contact[3], contact[4]))
 
-# JSON verilerini silme
-def delete_json_data(filename, key):
-    data = read_json_data(filename)
-    del data[key]
-    write_json_data(filename, data)
+# Kişileri Listeleme işlemi
+def list_contacts():
+    conn = sqlite3.connect("SQLData/mydatabase.db")
+    cursor = conn.cursor()
 
-# İsmetify verilerini okuma
-def read_ismetify_data(filename):
-    data = []
-    with open(filename, 'r') as file:
-        for line in file:
-            row = line.strip().split('|')
-            data.append(row)
-    return data
+    # Varsayılan olarak, ad ve soyada göre sırala
+    cursor.execute("SELECT * FROM contacts ORDER BY first_name, last_name")
 
-# İsmetify verilerini yazma
-def write_ismetify_data(filename, data):
-    with open(filename, 'w') as file:
-        for row in data:
-            line = '|'.join(row)
-            file.write(line + '\n')
+    contacts = cursor.fetchall()
+    conn.close()
 
-# İsmetify verilerini güncelleme
-def update_ismetify_data(filename, row_index, column_index, new_value):
-    data = read_ismetify_data(filename)
-    data[row_index][column_index] = new_value
-    write_ismetify_data(filename, data)
+    return contacts
+# SQLite veritabanından kişi silme fonksiyonu
+def delete_contact_from_sqlite(first_name, last_name, email):
+    conn = sqlite3.connect("SQLData/mydatabase.db")
+    cursor = conn.cursor()
 
-# İsmetify verilerini silme
-def delete_ismetify_data(filename, row_index):
-    data = read_ismetify_data(filename)
-    del data[row_index]
-    write_ismetify_data(filename, data)
+    cursor.execute("SELECT id FROM contacts WHERE first_name = ? AND last_name = ? AND email = ?", (first_name, last_name, email))
+    existing_contact = cursor.fetchone()
 
-from simple_term_menu import TerminalMenu
+    if existing_contact:
+        contact_id = existing_contact[0]
+        cursor.execute("DELETE FROM contacts WHERE id = ?", (contact_id,))
+        conn.commit()
+        conn.close()
+        print("Contact deleted successfully from SQLite.")
+    else:
+        print("Contact not found in SQLite.")
+
+# CSV verilerinden kişi silme fonksiyonu
+def delete_contact_from_csv(first_name, last_name, email):
+    with open("CSVData/contacts.csv", 'r') as file:
+        csv_data = list(csv.reader(file))
+
+    deleted = False
+    for row in csv_data:
+        if row[0] == first_name and row[1] == last_name and row[3] == email:
+            csv_data.remove(row)
+            deleted = True
+
+    if deleted:
+        with open("CSVData/contacts.csv", 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(csv_data)
+        print("Contact deleted successfully from CSV.")
+    else:
+        print("Contact not found in CSV.")
+
+# JSON verilerinden kişi silme fonksiyonu
+def delete_contact_from_json(first_name, last_name, email):
+    json_file = "JSONData/contacts.json"
+
+    if not os.path.exists(json_file):
+        print("Contact not found in JSON.")
+        return
+
+    with open(json_file, 'r') as file:
+        json_data = json.load(file)
+
+    deleted = False
+    for contact in json_data:
+        if contact.get("first_name") == first_name and contact.get("last_name") == last_name and contact.get("email") == email:
+            json_data.remove(contact)
+            deleted = True
+
+    if deleted:
+        with open(json_file, 'w') as file:
+            json.dump(json_data, file, indent=4)
+        print("Contact deleted successfully from JSON.")
+    else:
+        print("Contact not found in JSON.")
+
+# İsmetify verilerinden kişi silme fonksiyonu
+def delete_contact_from_ismetify(first_name, last_name, email):
+    ismetify_file = "IsmetifyData/contacts.txt"
+
+    if not os.path.exists(ismetify_file):
+        print("Contact not found in İsmetify.")
+        return
+
+    with open(ismetify_file, 'r') as file:
+        lines = file.readlines()
+
+    deleted = False
+    with open(ismetify_file, 'w') as file:
+        for line in lines:
+            contact_data = line.strip().split('|')
+            if contact_data[0] == first_name and contact_data[1] == last_name and contact_data[3] == email:
+                deleted = True
+            else:
+                file.write(line)
+
+    if deleted:
+        print("Contact deleted successfully from İsmetify.")
+    else:
+        print("Contact not found in İsmetify.")
+
+# Kişiyi tüm veri kaynaklarından silen fonksiyon
+def delete_contact_from_all_sources(first_name, last_name, email):
+    delete_contact_from_sqlite(first_name, last_name, email)
+    delete_contact_from_csv(first_name, last_name, email)
+    delete_contact_from_json(first_name, last_name, email)
+    delete_contact_from_ismetify(first_name, last_name, email)
+
+# Ana menü içindeki fonksiyonlar
+def edit_contact_menu():
+    contacts = list_contacts()
+    if not contacts:
+        print("Düzenlenecek kişi bulunamadı.")
+    else:
+        for index, contact in enumerate(contacts):
+            print("{}. Ad: {}, Soyad: {}, Telefon: {}, E-posta: {}".format(index, contact[1], contact[2], contact[3], contact[4]))
+        contact_index = int(input("Düzenlemek istediğiniz kişinin sıra numarasını girin 0'dan başlar. (q: İptal): "))
+        
+        if contact_index == 'q':
+            print("İptal edildi.")
+        elif 0 <= contact_index < len(contacts):
+            contact = contacts[contact_index]
+            first_name, last_name, phone_number, email = contact[1], contact[2], contact[3], contact[4]
+
+            # Düzenlenecek kişinin bilgilerini göster
+            print("Düzenleniyor: ")
+            print("Ad: {}".format(first_name))
+            print("Soyad: {}".format(last_name))
+            print("Telefon Numarası: {}".format(phone_number))
+            print("E-posta: {}".format(email))
+
+            # Yeni bilgileri al
+            new_first_name = input("Yeni Ad: ")
+            new_last_name = input("Yeni Soyad: ")
+            new_phone_number = input("Yeni Telefon Numarası: ")
+            new_email = input("Yeni E-posta: ")
+
+            # Kişiyi güncelle
+            delete_contact_from_all_sources(first_name, last_name, email)
+            add_contact_to_all_sources(new_first_name, new_last_name, new_phone_number, new_email)
+            print("Kişi başarıyla güncellendi.")
+        else:
+            print("Geçersiz bir sıra numarası girdiniz.")
 
 def main_menu():
     menu_title = "Ana Menü"
@@ -133,23 +262,30 @@ def main_menu():
     while True:
         selected_index = menu.show()
         if selected_index == 0:
-            # Kişi Ekle işlemi için gerekli kodu buraya ekleyin
-            pass
+            first_name = input("Ad: ")
+            last_name = input("Soyad: ")
+            phone_number = input("Telefon Numarası: ")
+            email = input("E-posta: ")
+            add_contact_to_all_sources(first_name, last_name, phone_number, email)
         elif selected_index == 1:
-            # Kişi Sil işlemi için gerekli kodu buraya ekleyin
-            pass
+            first_name = input("Ad: ")
+            last_name = input("Soyad: ")
+            email = input("E-posta: ")
+            delete_contact_from_all_sources(first_name, last_name, email)
         elif selected_index == 2:
-            # Kişileri Listele işlemi için gerekli kodu buraya ekleyin
-            pass
+            list_contacts_menu()
         elif selected_index == 3:
-            # Kişi Düzenle işlemi için gerekli kodu buraya ekleyin
-            pass
+            edit_contact_menu()
         elif selected_index == 4:
-            # Yedekten Geri Yükle işlemi için gerekli kodu buraya ekleyin
+            # Yedekten Geri Yükleme işlemi için gerekli kodu buraya ekleyin
             pass
         elif selected_index == 5:
             print("Çıkış yapılıyor...")
             break
 
+
 if __name__ == "__main__":
+    create_folders()
+    create_sqlite_database()
+    create_data_files()
     main_menu()
